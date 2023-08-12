49b171018bc1877e87aa5ecd8a936292d7eaf834
norely08 <nolyangel23@gmail.com>
2023-08-12 14:40:33 -0600

    vonfirmar cerrar sesion y bloquear la redireccion a otras paginas

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,6 +1,11 @@
 <?php
-session_start();
-session_destroy();
-header("Location: index.php");
+// Verificar si la sesión está activa
+if (session_status() === PHP_SESSION_ACTIVE) {
+    // Cerrar la sesión actual
+    session_destroy();
+}
+
+// Redirigir a la página de confirmación (opcional)
+header("Location: confirmation.php");
 exit;
 ?>
